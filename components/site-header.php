5f841b1f73191bcfe13c2796cdf94fedf9c3f608
Syret <kart.syret@khk.ee>
2014-09-30 09:41:28 +0300

    ÜL_1

--- components/site-header.php
+++ components/site-header.php
@@ -1,14 +1,20 @@
 <header id="site-header">
   <div class="site-title">
-    <h1>Arvestustöö</h1>
+    <h1>
+      <?php
+      $site_title = "Arvestustöö";
+      echo $site_title;
+      ?>
+    </h1>
   </div>
 
-  <nav class="menu-main">
+<nav class="menu-main">
     <ul class="menu">
-      <li><a href="index.php">Avaleht</a></li>
-      <li><a href="news.php">Uudised</a></li>
-      <li><a href="team.php">Meeskond</a></li>
-      <li><a href="exercises.php">Harjutused</a></li>
+        <?php
+            foreach ($menu_main as $attribute => $value){
+              echo '<li><a href="' . $value . '">' . $attribute . "</a></li>";
+            };
+        ?>
     </ul>
   </nav>
 </header>
